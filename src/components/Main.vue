030c5a5f30aecfe77cc7ef091b6d7948996562ff
박주완 <nowtop@nate.com>
2023-09-13 13:02:37 +0900

    commit

---
+++ src/components/Main.vue
@@ -0,0 +1,250 @@
+<template>
+  <div class="android-large">
+    <div class="div">
+      <div class="carousel">
+        <div class="indicator">
+          <div class="div-2"><img class="element" src="../assets/img/1-2-4.png" /></div>
+          <div class="div-2">
+            <div class="overlap-group"><div class="element-2"></div></div>
+          </div>
+          <div class="div-2">
+            <div class="overlap"><div class="element-2"></div></div>
+          </div>
+          <div class="div-2">
+            <div class="element-wrapper"><div class="element-2"></div></div>
+          </div>
+          <div class="div-2">
+            <div class="div-wrapper"><div class="element-2"></div></div>
+          </div>
+        </div>
+        <div class="frame">
+          <div class="source">
+            <div class="group">
+              <div class="overlap-group-2">
+                <img class="img" src="../assets/img/1-6-4.png" />
+                <div class="text-wrapper">CNK-01-XX-XX</div>
+                <p class="p">A building that can be used for any purpose</p>
+                <p class="element-m-floors">
+                  210.66 m²&nbsp;&nbsp;|&nbsp;&nbsp;2 Floors&nbsp;&nbsp;|&nbsp;&nbsp;5
+                  Bedrooms&nbsp;&nbsp;|&nbsp;&nbsp;4 Bathrooms
+                </p>
+                <div class="text-wrapper-2">488,700,000won</div>
+              </div>
+            </div>
+          </div>
+          <div class="source">
+            <div class="group">
+              <div class="overlap-group-2">
+                <img class="element-3" src="../assets/img/1-6-3.png" />
+                <div class="text-wrapper">CWK-01-XX-XX</div>
+                <p class="p">A building that can be used for any purpose</p>
+                <p class="element-m-floors">
+                  210.66 m²&nbsp;&nbsp;|&nbsp;&nbsp;2 Floors&nbsp;&nbsp;|&nbsp;&nbsp;5
+                  Bedrooms&nbsp;&nbsp;|&nbsp;&nbsp;4 Bathrooms
+                </p>
+                <div class="text-wrapper-2">488,700,000won</div>
+              </div>
+            </div>
+          </div>
+          <div class="source">
+            <div class="group">
+              <div class="overlap-group-3">
+                <div class="text-wrapper">PRO-01-XX-XX</div>
+                <p class="p">A building that can be used for any purpose</p>
+                <p class="element-m-floors">
+                  210.66 m²&nbsp;&nbsp;|&nbsp;&nbsp;2 Floors&nbsp;&nbsp;|&nbsp;&nbsp;5
+                  Bedrooms&nbsp;&nbsp;|&nbsp;&nbsp;4 Bathrooms
+                </p>
+                <div class="text-wrapper-2">488,700,000won</div>
+              </div>
+              <img class="element-4" src="../assets/img/1-9.png" />
+            </div>
+          </div>
+          <div class="source">
+            <div class="group">
+              <div class="overlap-group-3">
+                <div class="text-wrapper">CNK-02-XX-XX</div>
+                <p class="p">A building that can be used for any purpose</p>
+                <p class="element-m-floors">
+                  210.66 m²&nbsp;&nbsp;|&nbsp;&nbsp;2 Floors&nbsp;&nbsp;|&nbsp;&nbsp;5
+                  Bedrooms&nbsp;&nbsp;|&nbsp;&nbsp;4 Bathrooms
+                </p>
+                <div class="text-wrapper-2">488,700,000won</div>
+              </div>
+              <img class="element-5" src="../assets/img/1-9.png" />
+            </div>
+          </div>
+          <div class="source">
+            <div class="group">
+              <div class="overlap-group-3">
+                <div class="text-wrapper">CWK-03-XX-XX</div>
+                <p class="p">A building that can be used for any purpose</p>
+                <p class="element-m-floors">
+                  210.66 m²&nbsp;&nbsp;|&nbsp;&nbsp;2 Floors&nbsp;&nbsp;|&nbsp;&nbsp;5
+                  Bedrooms&nbsp;&nbsp;|&nbsp;&nbsp;4 Bathrooms
+                </p>
+                <div class="text-wrapper-2">488,700,000won</div>
+              </div>
+              <img class="element-6" src="../assets/img/1-9.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="overlap-2">
+        <div class="text-wrapper-3">M House</div>
+        <div class="group-wrapper">
+          <div class="group-2">
+            <div class="rectangle"></div>
+            <div class="rectangle-2"></div>
+            <div class="rectangle-3"></div>
+          </div>
+        </div>
+      </div>
+      <div class="group-3">
+        <div class="text-wrapper-4">새로운 모델</div>
+        <div class="text-wrapper-5">새로운 모델을 만나보세요</div>
+      </div>
+      <div class="overlap-3">
+        <div class="group-4">
+          <div class="overlap-4">
+            <div class="rectangle-4"></div>
+            <div class="rectangle-5"></div>
+            <div class="rectangle-6"></div>
+          </div>
+          <img class="element-7" src="../assets/img/1-9.png" />
+          <img class="element-8" src="../assets/img/1-9.png" />
+        </div>
+        <div class="group-5">
+          <div class="text-wrapper-6">베스트 모델</div>
+          <p class="text-wrapper-5">지금 가장 인기있는 모델을 만나보세요</p>
+        </div>
+        <div class="carousel-2">
+          <div class="frame-2">
+            <img class="source" src="../assets/img/source3-1.svg" />
+            <div class="img-wrapper"><img class="element-9" src="../assets/img/1-11-1.png" /></div>
+            <div class="img-wrapper"><img class="element-10" src="../assets/img/1-11.png" /></div>
+          </div>
+          <div class="group-6">
+            <div class="text-wrapper-7">298,370,000 Won</div>
+            <div class="text-wrapper-8">CNK-01-XX-XX</div>
+          </div>
+        </div>
+      </div>
+      <div class="overlap-wrapper">
+        <div class="overlap-5">
+          <p class="text-wrapper-9">Copyright 2023 ooooo.All Rights Reserved.</p>
+          <div class="text-wrapper-10">주식회사 ㅇㅇㅇㅇ</div>
+          <div class="flexcontainer">
+            <p class="text">
+              <span class="span">사업자등록번호 : 000-00-0000 | 대표자: 김인한<br /></span>
+            </p>
+            <p class="text">
+              <span class="span">[서울사무소]서울시 서초구 서초중앙로 188 아크로비스타 사무동 L-422호<br /></span>
+            </p>
+            <p class="text">
+              <span class="span">[기술연구소] 서울시 서초구 서초중앙로 188 아크로비스타 사무동 L-410호<br /></span>
+            </p>
+            <p class="text"><span class="span">대표전화: +82-2-595-0203</span></p>
+          </div>
+          <a href="https://www.youtube.com/@m3systems537" target="_blank" rel="noopener noreferrer"
+            ><div class="vector-wrapper"><img class="vector" src="../assets/img/vector-27.svg" /></div
+          ></a>
+          <div class="text-wrapper-11">M House</div>
+        </div>
+      </div>
+      <a href="https://m3systems.co.kr/use-case/plan" target="_blank" rel="noopener noreferrer"
+        ><div class="overlap-group-wrapper">
+          <div class="overlap-6">
+            <img class="image" src="../assets/img/image-6.png" />
+            <div class="rectangle-7"></div>
+            <div class="group-7">
+              <div class="overlap-group-4"><div class="text-wrapper-12">더 알아보기</div></div>
+            </div>
+            <div class="group-8">
+              <div class="text-wrapper-13">맞춤 주택 추천받기</div>
+              <p class="text-wrapper-14">나에게 딱 맞는 집은 어떤 집일까 ?</p>
+            </div>
+          </div>
+        </div></a
+      >
+      <div class="group-9">
+        <img class="element-11" src="../assets/img/buy-our-houses-now-8.png" />
+        <div class="text-wrapper-15">PRO-01-XX-XX</div>
+        <div class="text-wrapper-16">Buy our houses now!!!</div>
+        <div class="group-10">
+          <div class="ellipse"></div>
+          <div class="ellipse-2"></div>
+          <div class="ellipse-3"></div>
+          <div class="ellipse-4"></div>
+          <div class="ellipse-5"></div>
+        </div>
+      </div>
+      <div class="carousel-3">
+        <div class="frame-3">
+          <div class="div-wrapper-2">
+            <div class="group-11">
+              <div class="overlap-group-5"><img class="group-12" src="../assets/img/group-268-4.png" /></div>
+            </div>
+          </div>
+          <div class="div-wrapper-2">
+            <div class="overlap-7">
+              <div class="group-13">
+                <div class="overlap-group-6">
+                  <img class="CWK-XX-XX" src="../assets/img/buy-our-houses-now-8.png" />
+                  <img class="buy-our-houses-now" src="../assets/img/buy-our-houses-now-8.png" />
+                </div>
+              </div>
+              <div class="rectangle-8"></div>
+            </div>
+          </div>
+          <div class="div-wrapper-2">
+            <div class="overlap-7">
+              <div class="group-13">
+                <div class="overlap-group-7">
+                  <img class="CWK-XX-XX" src="../assets/img/buy-our-houses-now-8.png" />
+                  <img class="buy-our-houses-now" src="../assets/img/buy-our-houses-now-8.png" />
+                </div>
+              </div>
+              <div class="rectangle-8"></div>
+            </div>
+          </div>
+        </div>
+        <div class="indicator-2">
+          <div class="dot"></div>
+          <div class="dot-2"></div>
+          <div class="dot-2"></div>
+        </div>
+      </div>
+      <div class="group-14">
+        <img class="vector-2" src="../assets/img/vector-15.svg" /> <img class="vector-3" src="../assets/img/vector-16.svg" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
