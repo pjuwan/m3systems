099d81e3f0eb0e4ba43e6d7620d005f351bee284
박주완 <nowtop@nate.com>
2023-09-22 23:41:57 +0900

    모바일 풋터 CSS 수정

--- src/components/MobileFooter.vue
+++ src/components/MobileFooter.vue
@@ -2,24 +2,44 @@
   <div class="mobile-footer">
     <div class="overlap-wrapper">
       <div class="overlap-5">
-        <p class="text-wrapper-9">Copyright 2023 ooooo.All Rights Reserved.</p>
-        <div class="text-wrapper-10">주식회사 ㅇㅇㅇㅇ</div>
-        <div class="flexcontainer">
-          <p class="text">
-            <span class="span">사업자등록번호 : 000-00-0000 | 대표자: 김인한<br /></span>
-          </p>
-          <p class="text">
-            <span class="span">[서울사무소]서울시 서초구 서초중앙로 188 아크로비스타 사무동 L-422호<br /></span>
-          </p>
-          <p class="text">
-            <span class="span">[기술연구소] 서울시 서초구 서초중앙로 188 아크로비스타 사무동 L-410호<br /></span>
-          </p>
-          <p class="text"><span class="span">대표전화: +82-2-595-0203</span></p>
+        <div class="left">
+          <img class="rectangle" src="@/assets/img/rectangle-129.png" />
+          <div class="text-wrapper-10">주식회사 엠쓰리시스템즈</div>
+          <div class="flexcontainer">
+            <p class="text">
+              <span class="span">사업자등록번호 : 221-88-02146&nbsp;&nbsp; |&nbsp;&nbsp; 대표자: 김인한<br /></span>
+            </p>
+            <p class="text">
+              <span class="span">[서울사무소]서울시 서초구 서초중앙로 188 아크로비스타 사무동 L-422호<br /></span>
+            </p>
+            <p class="text">
+              <span class="span">[기술연구소] 서울시 서초구 서초중앙로 188 아크로비스타 사무동 L-410호<br /></span>
+            </p>
+            <p class="text"><span class="span">대표전화: +82-2-595-0203</span></p>
+          </div>
         </div>
-        <a href="https://www.youtube.com/@m3systems537" target="_blank" rel="noopener noreferrer"
-          ><div class="vector-wrapper"><img class="vector" src="@/assets/img/vector-27.svg" /></div
-        ></a>
-        <div class="text-wrapper-11">M House</div>
+        <div class="right">
+          <div class="box">
+            <div class="overlap-group-wrapper">
+              <div class="overlap-group">
+                <div class="ellipse-2"></div>
+                <div class="ellipse"></div>
+              </div>
+            </div>
+            <a href="https://www.youtube.com/@m3systems537" target="_blank" rel="noopener noreferrer">
+              <div class="vector-wrapper">
+                <img class="vector" src="@/assets/img/vector-27.svg" />
+              </div>
+            </a>
+            <img class="img" src="@/assets/img/rectangle-131.png" />
+          </div>
+          <p class="p">
+            <span class="text-wrapper-3">이용약관&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            <span class="text-wrapper-4">|</span>
+            <span class="text-wrapper-3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 개인정보처리방침침 </span>
+          </p>
+          <p class="text-wrapper-9">Copyright 2023 m3systems.All Rights Reserved.</p>
+        </div>        
       </div>
     </div>
     <div class="group-14">
@@ -38,3 +58,9 @@ export default {
   }
 }
 </script>
+<style scoped>
+  a {
+    display: inline-flex;
+    padding: 3px 0;    
+  }
+</style>
--- src/components/MobileHeader.vue
+++ src/components/MobileHeader.vue
@@ -1,38 +1,38 @@
 <template>
-<div class="mobile-header" :class="{overlap: showMenu}">
-  <div class="menu" v-if="!showMenu">
-    <div class="text-wrapper-3">M House</div>
-      <div class="group-wrapper" @click="showMenuList()">
-        <div class="group-2">
-        <div class="rectangle"></div>
-        <div class="rectangle-2"></div>
-        <div class="rectangle-3"></div>
+  <div class="mobile-header" :class="{overlap: showMenu}">
+    <div class="menu" v-if="!showMenu">
+      <div class="text-wrapper-3">M House</div>
+        <div class="group-wrapper" @click="showMenuList()">
+          <div class="group-2">
+          <div class="rectangle"></div>
+          <div class="rectangle-2"></div>
+          <div class="rectangle-3"></div>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="menu-kor" v-if="showMenu">
-    <div class="overlap-group-wrapper">
-      <div class="overlap-group">
-        <div class="group"><div class="text-wrapper">English</div></div>
-        <div class="div">
-          <div class="title">M House</div>
-          <div class="group-wrapper" @click="showMenuList()">
-            <div class="group-2">
-              <div class="rectangle"></div>
-              <div class="rectangle-2"></div>
-              <div class="rectangle-3"></div>
+    <div class="menu-kor" v-if="showMenu">
+      <div class="overlap-group-wrapper">
+        <div class="overlap-group">
+          <div class="group"><div class="text-wrapper">English</div></div>
+          <div class="div">
+            <div class="title">M House</div>
+            <div class="group-wrapper" @click="showMenuList()">
+              <div class="group-2">
+                <div class="rectangle"></div>
+                <div class="rectangle-2"></div>
+                <div class="rectangle-3"></div>
+              </div>
             </div>
           </div>
+          <div class="text-wrapper-2" @click="goMenu()">브랜드스토리</div>
+          <div class="text-wrapper-3">전체모델</div>
+          <div class="div-wrapper"><div class="text-wrapper">문의하기</div></div>
+          <div class="text-wrapper-4">프리미엄모델</div>
+          <div class="text-wrapper-5">스탠다드모델</div>
         </div>
-        <div class="text-wrapper-2" @click="goMenu()">브랜드스토리</div>
-        <div class="text-wrapper-3">전체모델</div>
-        <div class="div-wrapper"><div class="text-wrapper">문의하기</div></div>
-        <div class="text-wrapper-4">프리미엄모델</div>
-        <div class="text-wrapper-5">스탠다드모델</div>
       </div>
-    </div>
-  </div>  
-</div>
+    </div>  
+  </div>
 </template>
 <script>
 export default {
