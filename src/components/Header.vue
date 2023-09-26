cbd7ae81cc4b40315268d16ef88bf0ef9f579d84
박주완 <nowtop@nate.com>
2023-09-27 08:51:14 +0900

    header 스타일 수정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="header" :class="{ 'hovered': isHovered }">
+    <h2 class="logo" @click="goMenu('Home', '')">M House</h2>
     <div class="menu">
       <!--<img class="earth-globe" src="@/assets/img/earth-globe-1-1.png" />-->
-      <h2 class="logo" @click="goMenu('Home')">M House</h2>
-      <div class="name">
-        <span>브랜드스토리</span>
+      <div
+        class="name"
+        @click="goMenu('', '001')"
+      >
+        <span :class="{selected: menuId === '001'}">브랜드스토리</span>
       </div>
-      <div class="name submenu" @mouseover="isHovered = true" @mouseleave="isHovered = false">
-        <span>전체모델</span>
+      <div 
+        class="name submenu"
+        @mouseover="isHovered = true"
+        @mouseleave="isHovered = false"
+        @click="goMenu('', '002')"
+      >
+        <span :class="{selected: menuId === '002'}">전체모델</span>
         <div class="dropdown" :class="{ 'hovered': isHovered }" @mouseover="isHovered = true" @mouseleave="isHovered = false">
           <div class="name">프리미엄 모델</div>
           <div class="name">스탠다드 모델</div>
         </div>
       </div>
-      <div class="name" @click="goMenu('Question')">
-        <span>문의하기</span>
+      <div
+        class="name"
+        @click="goMenu('Question', '003')"
+      >
+        <span :class="{selected: menuId === '003'}">문의하기</span>
       </div>
     </div>
     <!--<img class="union" src="@/assets/img/union.png" />-->
@@ -25,11 +36,13 @@ export default {
   name: 'Header',
   data() {
     return {
-      isHovered: false
+      isHovered: false,
+      menuId: ''
     }
   },
   methods: {
-    goMenu(menuNm) {
+    goMenu(menuNm, menuId) {
+      this.menuId = menuId;
       this.$router.push({ name: menuNm });
     }
   }  
